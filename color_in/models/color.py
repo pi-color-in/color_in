28d91592da70aa9a1c79f94a032bce465f07f5bc
Mateus Nóbrega <mateusn@outlook.com>
2019-06-14 20:04:48 -0300

    (#12) - Adding docstring to the Color class and a validator to the `cmyk` field

--- color_in/models/color.py
+++ color_in/models/color.py
@@ -1,6 +1,13 @@
 from django.db import models
+from django.core.validators import validate_comma_separated_integer_list
 
 
 class Color(models.Model):
+    """
+        Represents a Color object
+
+        Fields: color_name, cmyk (comma-delimited list)
+    """
     color_name = models.CharField(max_length=100)
-    cmyk = models.CharField(max_length=20) # it's gonna work as a comma-delimited list
+    cmyk = models.CharField(max_length=20,
+                            validators=[validate_comma_separated_integer_list])
--- color_in/models/order.py
+++ color_in/models/order.py
@@ -1,5 +1,6 @@
 from django.db import models
 from .color import Color
 
+
 class Order(models.Model):
     demanded_colors = models.ManyToManyField(Color)
