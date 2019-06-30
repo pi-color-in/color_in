7308deeef312e32be80712572e06d8fd8c59fe85
Mateus Nóbrega <mateusn@outlook.com>
2019-06-30 15:35:37 -0300

    (#21) - Adding a warning message in the stock status and a template to the pages

--- color_in/models/stock.py
+++ color_in/models/stock.py
@@ -4,13 +4,13 @@ from django.core.validators import MinValueValidator, MaxValueValidator
 
 class Stock(models.Model):
     stock_base = models.IntegerField(verbose_name='Base (Branco)', validators=[
-                                     MinValueValidator(0), MaxValueValidator(100)])
+                                     MinValueValidator(0)])
     stock_black = models.IntegerField(verbose_name='Preto',
-                                      validators=[MinValueValidator(0), MaxValueValidator(100)])
+                                      validators=[MinValueValidator(0)])
     stock_yellow = models.IntegerField(verbose_name='Amarelo',
-                                       validators=[MinValueValidator(0), MaxValueValidator(100)])
-    stock_magenta = models.IntegerField(verbose_name='Magentas',
-                                        validators=[MinValueValidator(0), MaxValueValidator(100)])
+                                       validators=[MinValueValidator(0)])
+    stock_magenta = models.IntegerField(verbose_name='Magenta',
+                                        validators=[MinValueValidator(0)])
     stock_cyan = models.IntegerField(verbose_name='Ciano',
-                                     validators=[MinValueValidator(0), MaxValueValidator(100)])
+                                     validators=[MinValueValidator(0)])
     stock_status = models.BooleanField(verbose_name='Status')
--- color_in/views/stock_view.py
+++ color_in/views/stock_view.py
@@ -15,6 +15,34 @@ class StockObjectListView(ListView):
     queryset = Stock.objects.filter(pk=1)
 
 
+    def get_critical_colors(self):
+        """
+            Returns the colors whose quantity is critical (less or equal than 100ml)
+        """
+        critical_level = 100
+        obj = Stock.objects.get(pk=1)
+
+        all_levels = [(obj.stock_base, 'Branco'), (obj.stock_black, 'Preto'),
+                      (obj.stock_yellow, 'Amarelo'), (obj.stock_magenta, 'Magenta'),
+                      (obj.stock_cyan, 'Ciano')]
+
+        # get all colors with critical level 
+        critical_colors = filter(lambda tup: tup[0] <= critical_level, all_levels)
+
+        # we're gonna just return their names so filter the level out
+        critical_colors = list(map(lambda tup: tup[1], critical_colors))
+
+        return critical_colors
+
+    def get_context_data(self, **kwargs):
+        data = super().get_context_data(**kwargs)
+
+        critical_colors = self.get_critical_colors()
+        data['critical_colors_list'] = critical_colors
+        data['critical_colors'] = ", ".join(critical_colors)
+
+        return data
+
 class StockObjectUpdateView(UpdateView):
     model = Stock
     queryset = Stock.objects.filter(pk=1)
