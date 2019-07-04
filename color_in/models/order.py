a93b2581d9fec55123058788752287fadac2f906
Mateus Nóbrega <mateusn@outlook.com>
2019-07-04 20:00:51 -0300

    (#4) - Changing model

--- color_in/models/order.py
+++ color_in/models/order.py
@@ -3,4 +3,8 @@ from .color import Color
 
 
 class Order(models.Model):
-    demanded_colors = models.ManyToManyField(Color)
+    """
+        Represents an order in the application
+    """
+    ordered_color = models.ForeignKey(Color, on_delete=models.DO_NOTHING, blank=False)
+    quantity = models.IntegerField(verbose_name="Quantidade", blank=False)
